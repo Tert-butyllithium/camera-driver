3326174bf35097c4f22040c6080c728ae064feab
Tert-butyllithium <lihaonan1105@gmail.com>
2021-12-29 20:33:33 +0800

    add more timers

--- take_picture.cpp
+++ take_picture.cpp
@@ -7,7 +7,7 @@
 using std::min;
 
 // camera_VC0706 cam = camera_VC0706(&cameraconnection);
-SerialPort uart = SerialPort();
+SerialPort uart = SerialPort(115200,"/dev/ttySIF1");
 
 VC0706 cam = VC0706(&uart);
 void setup()
@@ -38,9 +38,9 @@ void setup()
 
     // sleep(3);
 
-    start = time(NULL);
-    cam.setImageSize(VC0706_640x480);
-    end = time(NULL);
+    // start = time(NULL);
+    // cam.setImageSize(VC0706_640x480);
+    // end = time(NULL);
     printf("set resolution 640 x 480\n");
     printf("set resolution time %lu\n",end-start);
     //cam.setImageSize(VC0706_320x240);
@@ -60,16 +60,22 @@ void setup()
 
 void loop()
 {
+    time_t start = time(NULL);
     if (!cam.takePicture())
         printf("Failed to snap!\n");
     else
         printf("Picture taken!\n");
+    time_t end = time(NULL);
     char filename[13];
+    printf("taking picture %lu\n",end-start);
     strcpy(filename, "IMAGE00.JPG");
     FILE* imgFile = fopen(filename, "wb");
+    
+    start = time(NULL);
     uint32_t jpglen = cam.frameLength();
+    end = time(NULL);
 
-    printf("image size: %u bytes\n", jpglen);
+    printf("image size: %u bytes, time: %lu\n", jpglen, end-start);
 
     printf("Writing image to %s\n",filename);
 
