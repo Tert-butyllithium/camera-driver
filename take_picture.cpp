bb6315d9808c7bf68357b7689092579eb8141ad1
Tert-butyllithium <lihaonan1105@gmail.com>
2021-12-30 16:05:01 +0800

    add inspect

--- take_picture.cpp
+++ take_picture.cpp
@@ -2,12 +2,12 @@
 #include <algorithm>
 #include <cstdio>
 #include <cstring>
-#include <unistd.h>
 #include <time.h>
+#include <unistd.h>
 using std::min;
 
 // camera_VC0706 cam = camera_VC0706(&cameraconnection);
-SerialPort uart = SerialPort(115200,"/dev/ttySIF1");
+SerialPort uart = SerialPort(115200, "/dev/ttySIF1");
 
 VC0706 cam = VC0706(&uart);
 void setup()
@@ -27,12 +27,12 @@ void setup()
     time_t start = time(NULL);
     char* reply = cam.getVersion();
     time_t end = time(NULL);
-    printf("get reply time %lu\n",end-start);
+    printf("get reply time %lu\n", end - start);
     if (reply == 0) {
         printf("Failed to get version");
     } else {
         printf("-----------------\n");
-        printf("%s\n",reply);
+        printf("%s\n", reply);
         printf("-----------------\n");
     }
 
@@ -42,9 +42,9 @@ void setup()
     // cam.setImageSize(VC0706_640x480);
     // end = time(NULL);
     printf("set resolution 640 x 480\n");
-    printf("set resolution time %lu\n",end-start);
-    //cam.setImageSize(VC0706_320x240);
-    //cam.setImageSize(VC0706_160x120);
+    printf("set resolution time %lu\n", end - start);
+    // cam.setImageSize(VC0706_320x240);
+    // cam.setImageSize(VC0706_160x120);
 
     // uint8_t imgsize = cam.getImageSize();
     // printf("Image size: %02X\n", imgsize);
@@ -67,17 +67,17 @@ void loop()
         printf("Picture taken!\n");
     time_t end = time(NULL);
     char filename[13];
-    printf("taking picture %lu\n",end-start);
+    printf("taking picture %lu\n", end - start);
     strcpy(filename, "IMAGE00.JPG");
     FILE* imgFile = fopen(filename, "wb");
-    
+
     start = time(NULL);
     uint32_t jpglen = cam.frameLength();
     end = time(NULL);
 
-    printf("image size: %u bytes, time: %lu\n", jpglen, end-start);
+    printf("image size: %u bytes, time: %lu\n", jpglen, end - start);
 
-    printf("Writing image to %s\n",filename);
+    printf("Writing image to %s\n", filename);
 
     while (jpglen > 0) {
         // read 2048 bytes each time
@@ -94,8 +94,36 @@ void loop()
     cam.resumeVideo();
 }
 
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define UART_REG_ADDR 0x10011000
+#define UART_REG_SIZE 0x400
+void* base;
+void inspect()
+{
+    int fd = open("/dev/mem", O_RDWR);
+    printf("fd: %d, offest: %lx\n", fd, UART_REG_ADDR);
+    base = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, UART_REG_ADDR);
+
+    unsigned int* txctrl = (unsigned int*)(base + 0x08);
+    unsigned int* rxctrl = (unsigned int*)(base + 0x0c);
+    unsigned int* div = (unsigned int*)(base + 0x18);
+
+    printf("txctrl: 0x%x\n", *txctrl);
+    printf("rxctrl: 0x%x\n", *txctrl);
+    printf("div: 0x%x\n", *txctrl);
+
+    munmap(base, 0x1000);
+}
+
 int main()
 {
+#ifdef INSPECT
+    inspect();
+#else
     setup();
     loop();
+#endif
 }
